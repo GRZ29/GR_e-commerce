3aa2ff64417bb90b77e02ae861ecb30fcce5f96c
Gerald RZ <rosalesgerald198@gmail.com>
2022-07-23 00:10:45 -0600

    ...

--- GR.System.Services/GR.System.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GR.System.Models
-{
-    public class Class1
-    {
-    }
-}
--- GR.System.Services/GR.System.Utility/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GR.System.Utility
-{
-    public class Class1
-    {
-    }
-}
