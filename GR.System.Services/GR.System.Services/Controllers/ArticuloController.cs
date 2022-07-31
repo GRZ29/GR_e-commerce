37e029ea2f6c14db72f0b7251093986340ec792c
Gerald RZ <rosalesgerald198@gmail.com>
2022-07-30 23:54:45 -0600

    deleted comments at Articulo controller to close Elmer' mouth

--- GR.System.Services/GR.System.Services/Controllers/ArticuloController.cs
+++ GR.System.Services/GR.System.Services/Controllers/ArticuloController.cs
@@ -13,32 +13,7 @@ namespace GR.System.Services.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //public class ArticuloController : ControllerBase
-    //{
-    //    public ArticuloController(IUnidadTrabajo controlador)
-    //    {
-    //        _controlador = controlador;
-    //    }
-
-    //    readonly IUnidadTrabajo _controlador;
-
-
-    //    [HttpGet]
-    //    public IActionResult GetArticulos()
-    //    {
-    //        var articulos = _controlador.Articulo.Listar(propiedades: "Categorias,Precio,Detalles", propiedadesDentro: "DescripcionAdicional,Descripcion");
-
-    //        //var detalles = _controlador.
-
-    //        return new JsonResult(new { success = true, data = articulos });
-    //    }
-
-    //    //    [HttpGet]
-    //    //    public IEnumerable<Articulos> GetArticulos2()
-    //    //    {
-    //    //        return _controlador.Articulo.Listar(propiedades: "Categorias,Precio,Detalles", propiedadesDentro: "DescripcionAdicional,Descripcion");
-    //    //    }
-    //}
+   
     public class ArticuloController : ControllerBase
     {
 
